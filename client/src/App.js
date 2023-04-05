b163d4c1603f4f250238d81848d1290429257d66
Harshit Sharma <harshit2112003@gamil.com>
2023-04-06 00:29:02 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+
 import "./style.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
